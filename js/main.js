35d5830131642a923a283225ae18ea1a7fb0cfe6
Alan Velasco <alansvela2019@gmail.com>
2024-10-17 17:59:33 -0500

    ciclos

---
+++ js/main.js
@@ -0,0 +1,18 @@
+document.write("hola mundo");
+document.write("hola mundo");
+document.write("hola mundo" + "<br>" );
+
+/*For es un ciclo el cual tiene 3 parametros
+la funcion es repetir un bloque de coddigo las veces necesarias 
+for (condición){
+    bloque de codigo 
+} */
+for(i=3 ; i<=15; i=i++){
+
+    //document.write( "Hola desde js" + "<br>");
+    //document.write(i+"<br>");
+    if
+        (i%2===0){
+            document.write("número par: " + i + "<br>" );
+        }
+}
